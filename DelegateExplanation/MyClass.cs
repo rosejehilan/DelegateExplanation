e4ce9ee96504bdaf6933b740f9813104570e7912
rosejehilan <44560523+rosejehilan@users.noreply.github.com>
2023-04-08 20:28:16 -0500

    Add project files.

---
+++ DelegateExplanation/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExplanation
+{
+    public class MyClass
+    {
+        public delegate void CallBack(int i);
+        public void LongRunning(CallBack obj)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                obj(i);
+            }
+        }
+    }
+}
---
+++ DelegateExplanation/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using DelegateExplanation;
+
+Console.WriteLine("Hello, World!");
+MyClass obj =  new MyClass();
+obj.LongRunning(Callback);
+
+static void Callback(int i)
+{
+    Console.WriteLine(i);
+}
+
